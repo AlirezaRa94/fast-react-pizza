da439b4d09dc2f850e5765026ceff1db1675b8bc
Alireza Raei <alirezaraei17@gmail.com>
2024-09-04 19:11:00 +1000

    feat: Add data loading and error handling for Order component
    - Added loader function to fetch order data in Order component
    - Updated App.jsx to use orderLoader for the /order/:orderId route
    - Integrated error handling for Order route using Error component

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,5 +1,12 @@
+import { getOrder } from "../../services/apiRestaurant";
+
 function Order() {
   return <h1>order</h1>;
+}
+
+export async function loader({ params }) {
+  const order = getOrder(params.orderId);
+  return order;
 }
 
 export default Order;
