0384c3bb9809a830b36912393442c9bee0718450
Alireza Raei <alirezaraei17@gmail.com>
2024-09-04 19:29:48 +1000

    feat: Display order details in Order component
    - Updated Order component to display order status, delivery time, and prices
    - Used helper functions to format currency, date, and calculate minutes left
    - Corrected currency format in helpers.js to use USD

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,7 +1,51 @@
+import { useLoaderData } from "react-router-dom";
+
 import { getOrder } from "../../services/apiRestaurant";
+import {
+  calcMinutesLeft,
+  formatCurrency,
+  formatDate,
+} from "../../utils/helpers";
 
 function Order() {
-  return <h1>order</h1>;
+  const order = useLoaderData();
+
+  // Everyone can search for all orders
+  // So for privacy reasons we're gonna exclude names or address
+  // these are only for the restaurant staff
+  const { status, priority, priorityPrice, orderPrice, estimatedDelivery } =
+    order;
+  const deliveryIn = calcMinutesLeft(estimatedDelivery);
+
+  console.log(priority);
+
+  return (
+    <div>
+      <div>
+        <h2>Status</h2>
+
+        <div>
+          {priority && <span>Priority </span>}
+          <span>{status} order</span>
+        </div>
+      </div>
+
+      <div>
+        <p>
+          {deliveryIn >= 0
+            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+            : "Order should have arrived"}
+        </p>
+        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+      </div>
+
+      <div>
+        <p>Price pizza: {formatCurrency(orderPrice)}</p>
+        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
+        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      </div>
+    </div>
+  );
 }
 
 export async function loader({ params }) {
