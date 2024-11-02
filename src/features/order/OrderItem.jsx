bbb3fc633c132e5338cf54ff6bb1ec5a022e0b46
Alireza Raei <alirezaraei17@gmail.com>
2024-11-02 22:36:00 +1100

    feat: Display order details and items in Order component
    - Added OrderItem component to display individual items in an order
    - Updated Order component to show order status, delivery time, and item list
    - Applied Tailwind CSS classes for better layout and styling

---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,28 @@
+import propTypes from "prop-types";
+
+import { formatCurrency } from "../../utils/helpers";
+
+OrderItem.propTypes = {
+  item: propTypes.shape({
+    quantity: propTypes.number.isRequired,
+    name: propTypes.string.isRequired,
+    totalPrice: propTypes.number.isRequired,
+  }).isRequired,
+};
+
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li className="py-3">
+      <div className="flex items-center justify-between gap-4 text-sm">
+        <p>
+          <span className="font-bold">{quantity}&times;</span> {name}
+        </p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
