36bd98c48bb2a568c3d4ee842be5a68b2d85e07a
Alireza Raei <alirezaraei17@gmail.com>
2024-11-08 00:05:34 +1100

    feat: Integrate address fetching in CreateOrder component
    - Added address fetching functionality using fetchAddress thunk
    - Updated CreateOrder component to handle address fetching and display errors
    - Disabled order button while address is loading

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,11 +1,11 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 
 import { createOrder } from "../../services/apiRestaurant";
 import { isValidPhone } from "../../utils/helpers";
 import Button from "../../ui/Button";
-import { getUsername } from "../user/userSlice";
+import { fetchAddress } from "../user/userSlice";
 import { clearCart, getCart, getTotalPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
@@ -17,15 +17,29 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
+  const dispatch = useDispatch();
 
   const cart = useSelector(getCart);
-  const username = useSelector(getUsername);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: addressError,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === "loading";
 
   const totalCartPrice = useSelector(getTotalPrice);
   const priorityPrice = withPriority ? Math.round(totalCartPrice * 0.2) : 0;
   const totalPrice = totalCartPrice + priorityPrice;
 
   if (cart.length === 0) return <EmptyCart />;
+
+  function handleGetPosition(e) {
+    e.preventDefault();
+
+    dispatch(fetchAddress());
+  }
 
   return (
     <div className="px-4 py-6">
@@ -59,11 +73,35 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
-            <input type="text" name="address" required className="input" />
+            <input
+              type="text"
+              name="address"
+              required
+              className="input"
+              defaultValue={address}
+              disabled={isLoadingAddress}
+            />
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {addressError}
+              </p>
+            )}
           </div>
+
+          {!position.latitude && !position.longitue && (
+            <span className="absolute right-[3px] top-[35px] z-50 sm:top-[3px] md:right-[5px] md:top-[5px]">
+              <Button
+                type="small"
+                onClick={handleGetPosition}
+                disabled={isLoadingAddress}
+              >
+                {isLoadingAddress ? "Loading..." : "Get Position"}
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -82,7 +120,7 @@ function CreateOrder() {
 
         <div className="flex items-center gap-3 md:gap-6">
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting ? "Placing order..." : "Order Now"}
           </Button>
           <p className="mt-2 text-sm text-stone-500">
