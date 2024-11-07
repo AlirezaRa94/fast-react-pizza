5183d97b954a3fb859e84590294d158c466e4fda
Alireza Raei <alirezaraei17@gmail.com>
2024-11-07 18:55:07 +1100

    feat: Add priority option and display total price in CreateOrder
    - Added withPriority state to handle priority checkbox
    - Calculated total price including priority price
    - Displayed total price in CreateOrder component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,15 +1,18 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 import { createOrder } from "../../services/apiRestaurant";
 import { isValidPhone } from "../../utils/helpers";
 import Button from "../../ui/Button";
 import { getUsername } from "../user/userSlice";
-import { clearCart, getCart } from "../cart/cartSlice";
+import { clearCart, getCart, getTotalPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
+import { formatCurrency } from "../../utils/helpers";
 
 function CreateOrder() {
+  const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
@@ -17,6 +20,10 @@ function CreateOrder() {
 
   const cart = useSelector(getCart);
   const username = useSelector(getUsername);
+
+  const totalCartPrice = useSelector(getTotalPrice);
+  const priorityPrice = withPriority ? Math.round(totalCartPrice * 0.2) : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
 
   if (cart.length === 0) return <EmptyCart />;
 
@@ -64,6 +71,8 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
+            checked={withPriority}
+            onChange={() => setWithPriority(!withPriority)}
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
           />
           <label htmlFor="priority" className="font-medium">
@@ -71,11 +80,14 @@ function CreateOrder() {
           </label>
         </div>
 
-        <div>
+        <div className="flex items-center gap-3 md:gap-6">
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
             {isSubmitting ? "Placing order..." : "Order Now"}
           </Button>
+          <p className="mt-2 text-sm text-stone-500">
+            {`Total Price: ${formatCurrency(totalPrice)}`}
+          </p>
         </div>
       </Form>
     </div>
